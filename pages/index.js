b1d1b1571acb21ab74eabc0c47d039e96b7ff409
twinkangaroos <twinkangaroos@gmail.com>
2023-06-27 12:20:47 +0900

    amplify deployエラー対応

--- pages/index.js
+++ pages/index.js
@@ -16,7 +16,7 @@ const Index = () => {
         <div className="profile">
           <div>
             <h2>JavaScript Nerd</h2>
-            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
           </div>
           <div className="image-container">
             <Image src="/images/profile.jpg" alt="hero" fill sizes="(max-width: 800px) 100vw" quality={90} priority />
